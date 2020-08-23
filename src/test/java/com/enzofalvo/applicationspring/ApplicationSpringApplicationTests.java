94491a02cbeafd689fbf6c6ddae6b13886b17254
Enzo Falvo <enzo.fvo@gmail.com>
2020-08-22 23:15:31 -0300

    Project created

---
+++ src/test/java/com/enzofalvo/applicationspring/ApplicationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzofalvo.applicationspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
