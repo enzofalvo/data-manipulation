157697d20f8a7ad0ebc88e939b0f2c613b29a5ca
Enzo Falvo <enzo.fvo@gmail.com>
2020-10-12 15:08:28 -0300

    User insert

--- src/main/java/com/enzofalvo/applicationspring/resources/UserResource.java
+++ src/main/java/com/enzofalvo/applicationspring/resources/UserResource.java
@@ -3,13 +3,17 @@ package com.enzofalvo.applicationspring.resources;
 
 import com.enzofalvo.applicationspring.entities.User;
 import com.enzofalvo.applicationspring.services.UserService;
+import java.net.URI;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -31,6 +35,10 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
     
-    
-    
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj) {
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
 }
--- src/main/java/com/enzofalvo/applicationspring/services/UserService.java
+++ src/main/java/com/enzofalvo/applicationspring/services/UserService.java
@@ -22,9 +22,10 @@ public class UserService {
         Optional <User> obj = repository.findById(id);
         return obj.get();
     }
-
     
-    
+    public User insert (User obj) {
+        return repository.save(obj);
+    }
 }
 
 
